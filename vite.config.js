28d8ce9a7a21782e54c5dda4f6735eec8e40e586
johnnieLopez <johnnie.lopez@foxordering.com>
2025-04-16 17:22:27 -0500

    fix typo

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import { resolve } from 'path';
 // https://vite.dev/config/
-var repositoryName = 'managament-dashboard';
+// const repositoryName = 'managament-dashboard';
 export default defineConfig({
     plugins: [react(), tailwindcss()],
-    base: "/".concat(repositoryName, "/"),
+    base: "/",
     resolve: {
         alias: {
             "@": resolve(__dirname, './src'),
--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,10 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { resolve } from 'path';
 // https://vite.dev/config/
-const repositoryName = 'managament-dashboard';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base:`/${repositoryName}/`,
+  base:`/`,
   resolve:{
     alias: {
       "@":resolve(__dirname, './src'),

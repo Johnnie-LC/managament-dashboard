19fea153790fe4fc5e08eb6d76174db046ee30a3
johnnieLopez <johnnie.lopez@foxordering.com>
2025-04-16 17:16:55 -0500

    fix type

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import { resolve } from 'path';
+// https://vite.dev/config/
+var repositoryName = 'managament-dashboard';
+export default defineConfig({
+    plugins: [react(), tailwindcss()],
+    base: "/".concat(repositoryName, "/"),
+    resolve: {
+        alias: {
+            "@": resolve(__dirname, './src'),
+            '@styles': resolve(__dirname, './src/styles'),
+            '@routes': resolve(__dirname, './src/routes'),
+            '@presentation': resolve(__dirname, './src/presentation'),
+            '@domain': resolve(__dirname, './src/domain'),
+            "@application": resolve(__dirname, './src/application'),
+            "@infrastructure": resolve(__dirname, './src/infrastructure'),
+            "@stores": resolve(__dirname, './src/presentation/stores'),
+            "@components": resolve(__dirname, './src/presentation/components')
+        }
+    }
+});
